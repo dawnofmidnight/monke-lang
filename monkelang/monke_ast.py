f73f306cff0f6570d48ea6b426a809835a595d60
DarkDragon <55249025+Arghya-AB@users.noreply.github.com>
2021-03-29 08:22:33 +0530

    My bad

--- monkelang/monke_ast.py
+++
@@ -1,9 +0,0 @@
-class FunctionCall:
-    def __init__(self, name, arguments):
-        self.name = name
-        self.arguments = arguments
-
-
-class Expr:
-    def __init__(self, value):
-        self.value = value
